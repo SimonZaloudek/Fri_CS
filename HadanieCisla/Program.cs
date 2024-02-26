4660f0a02a16abfc626ba8ab85a25239728ed43f
SimonZaloudek <simon.22.zaloudek@gmail.com>
2024-02-26 21:11:38 +0100

    init

---
+++ HadanieCisla/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+class Program() {
+    static Random random = new Random();
+
+      public static void Main(string[] args) {
+        int randomNum = random.Next(1,100);
+        int num = 0;
+
+        Console.WriteLine("Hadaj cislo od 1 do 100");
+        Core(num, randomNum);
+    } 
+
+     private static void Core(int num, int randomNum) {
+        int pocetHadani = 0;
+        while (randomNum != num) {
+            num = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (num < randomNum) {
+                Console.WriteLine("vacsie");
+                pocetHadani++;
+            }
+            if (num > randomNum) {
+                Console.WriteLine("mensie");
+                pocetHadani++;
+
+            }
+            if (num == randomNum) { 
+                Console.WriteLine("uhadol si, pocet hadani: " + pocetHadani);    
+            }
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("lukasstarinsky8@gmail.com");
+var time = DateTime.Now;
+var hour = time.Hour;
+if (hour >= 4 && hour <= 8) {
+    Console.WriteLine("Good morning");    
+} 
+if (hour > 8 && hour <= 18) {
+    Console.WriteLine("Good day");    
+}
+if (hour > 18 && hour <= 22) {
+    Console.WriteLine("Good evening");    
+}
+if (hour > 22 && hour < 4) {
+    Console.WriteLine("Good night");    
+}
