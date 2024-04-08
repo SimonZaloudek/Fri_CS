273f2a1f6b45fad52598d9156cd235c3ffe61da3
SimonZaloudek <simon.22.zaloudek@gmail.com>
2024-04-09 00:40:45 +0200

    dead

--- DU_1_C#NET/AddressBook.ViewerConsoleApp/Viewer.cs
+++ DU_1_C#NET/AddressBook.ViewerConsoleApp/Viewer.cs
@@ -1,39 +1,75 @@
-﻿using AddressBook.ViewerConsoleApp;
-using AdressBook.CommonLibrary;
+﻿using AdressBook.CommonLibrary;
 using System.Linq;
 
 public class Viewer 
 {
+    readonly string[] commands = { "input", "name", "position", "main-workplace", "output", "exit" };
+    private SearchResult searchResult;
+    private EmployeeList empList;
+
     public Viewer()
     {
-        Commands commands = new Commands();
-        string? input;
-
-        Console.WriteLine("Welcome to FRI database...");
-        Console.Write("List of all commands: ");
-        for (int i = 0; i < commands.getCommands().Length; i++) 
+        while (true)
         {
-            Console.Write($"{commands.getCommands()[i]}" + ", ");
-        }
-        Console.WriteLine(" ,--exit");
-
-        Console.WriteLine();
-        input = Console.ReadLine();
-        if (input != null)
-        {
-            string[] wholeInput = input.Split(" ");
-            for (int i = 0; i < wholeInput.Length; i++)
+            Console.WriteLine("Welcome to FRI employee database...");
+            foreach (string com in commands) 
             {
-                foreach (string command in commands.getCommands()) 
-                {
-                    if (wholeInput[i] == command)
-                    {
-                        commands.Execute(command, wholeInput[i + 1]);
-                    }
-                }
+                Console.Write("--" + com + ", ");
             }
+            Console.WriteLine();
+
+            Console.Write("Enter command: ");
+            string? input = Console.ReadLine();
+            if (input == "--exit") 
+            {
+                break;
+            }
+
+
+            string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Process the commands (every odd-indexed word)
+            for (int i = 0; i < words.Length; i += 2)
+            {
+                string? command = words[i];
+                string parameter = (i + 1 < words.Length) ? words[i + 1] : null;
+
+                // Remove quotes if present
+                if (!string.IsNullOrEmpty(parameter) && parameter.StartsWith("\"") && parameter.EndsWith("\""))
+                {
+                    parameter = parameter.Substring(1, parameter.Length - 2);
+                }
+
+                Execute(command, parameter);
+            }
+            Console.WriteLine();
         }
-        commands.SResult.WriteToConsole();
+
+    }
+
+    static void Execute(string command, string? parameter)
+    {
+        switch (command)
+        {
+            case "--input":
+                empList = empList.LoadFromJson(new FileInfo(parameter));
+                break;
+            case "--name":
+                Console.WriteLine(parameter);
+                break;
+            case "--position":
+                Console.WriteLine(parameter);
+                break;
+            case "--main-workplace":
+                Console.WriteLine(parameter);
+                break;
+            case "--output":
+                Console.WriteLine(parameter);
+                break;
+            default:
+                Console.WriteLine("Unknown command: " + command);
+                break;
+        }
     }
 
     public static void Main(string[] args) 
